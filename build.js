1a7f5d4b7c98458874d2557c9cd7acc572b996cf
Youssouf Elazizi <youssoufelazizi@gmail.com>
2018-04-08 21:18:06 +0100

    remove confuse between Bili babel config and parcel config

--- build.js
+++ build.js
@@ -7,39 +7,59 @@ const Bili = require("bili");
 
 const options = {
   input: "src/index.js",
-  outDir: "dist",
-  name: "StoryRouter",
-  format: ["es", "cjs"],
+  outDir: "lib",
+  name: "index",
+  format: ["es"],
   postcss: { extract: true },
   banner: true,
   external: false
 };
 
+// some confuse between babel config for parcel that use v6 and Bili that's use V7
+const babelBiliConfig = {
+  presets: ["@babel/preset-react"],
+  plugins: ["@babel/plugin-proposal-class-properties"]
+};
+const babelParcelConfig = {
+  presets: ["env", "react"],
+  plugins: ["transform-class-properties"]
+};
+
 let promise = Promise.resolve();
 
+// and use babel config V7
 // Clean up the output directory
-promise = promise.then(() => del(["dist/*"]));
+
+promise = promise.then(() => {
+  fs.writeFileSync(
+    "./.babelrc",
+    JSON.stringify(babelBiliConfig, null, "  "),
+    "utf-8"
+  );
+  del(["lib/*"]);
+});
 
 // Copy package.json and LICENSE
 promise = promise.then(() => {
   Bili.write(options).then(() => {
     console.log("Done!");
+    fs.writeFileSync(
+      "./.babelrc",
+      JSON.stringify(babelParcelConfig, null, "  "),
+      "utf-8"
+    );
   });
   delete pkg.devDependencies;
   delete pkg.scripts;
 
   fs.writeFileSync(
-    "dist/package.json",
+    "lib/package.json",
     JSON.stringify(pkg, null, "  "),
     "utf-8"
   );
+  fs.writeFileSync("lib/LICENSE", fs.readFileSync("LICENSE", "utf-8"), "utf-8");
   fs.writeFileSync(
-    "dist/LICENSE",
-    fs.readFileSync("LICENSE", "utf-8"),
-    "utf-8"
-  );
-  fs.writeFileSync(
-    "dist/README.md",
+    "lib/README.md",
     fs.readFileSync("README.md", "utf-8"),
     "utf-8"
   );
